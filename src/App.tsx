a360f70963288dec4a9acd1af05d7bc9e07e0159
Fernando <fernando.fnn97@gmail.com>
2023-02-07 21:05:42 -0300

    feat: add styled components
    - create ButtonContainer with styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { Button } from "./components/Button";
+
 export function App() {
-  return <div>App component</div>;
+  return (
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
+    </>
+  );
 }
