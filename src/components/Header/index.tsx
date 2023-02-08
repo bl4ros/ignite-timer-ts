adfa88e82592d8a6a38218de8d45dcd4247610d9
Fernando <fernando.fnn97@gmail.com>
2023-02-08 19:45:53 -0300

    feat: create header component and styling files

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import { HeaderContainer } from "./styles";
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <span>logo</span>
+      <nav>
+        <a href="">
+          timer
+        </a>
+      </nav>
+      <nav>
+        <a href="">
+          timer
+        </a>
+      </nav>
+    </HeaderContainer>
+  );
+}
